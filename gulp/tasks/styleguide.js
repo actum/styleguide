fc4d96597dab93729c69f48b04bb09efd141fb9c
Ronald Ružička <ronald.ruzicka@actum.cz>
2016-09-02 15:16:46 +0200

    use kss-config for styleguide configuration

--- gulp/tasks/styleguide.js
+++ gulp/tasks/styleguide.js
@@ -3,25 +3,11 @@ import { argv } from 'yargs';
 import config from '../config';
 import kss from 'kss';
 import browserSync from 'browser-sync';
-// import kssConfig from '../../kss-config.json';
+import kssConfig from '../../kss-config.json';
 
 const { src, dist, styleguide } = config.paths;
 const names = config.names;
 const isDev = argv.dev || false;
-const kssConfig = {
-    // The source and destination paths are relative to this file
-    source: [
-        './boilerplate'
-    ],
-    destination: './styleguide',
-    template: './dist',
-    // The css and js paths are URLs relative to the generated style guide
-    css: [
-        '../bootstrap.min.css'
-    ],
-    copyCss: true,
-    js: []
-};
 
 gulp.task('styleguide', () => {
     kss(kssConfig, () => {
